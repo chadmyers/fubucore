41e710b97ae3a990cf2fa5f6270f42b213c5b7f1
Chad Myers <chad.myers@gmail.com>
2011-10-31 13:52:08 -0400

    Added reader/writer lock semantics to Cache

--- src/FubuCore/Util/Cache.cs
+++ src/FubuCore/Util/Cache.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace FubuCore.Util
 {
     [Serializable]
     public class Cache<TKey, TValue> : IEnumerable<TValue>
     {
-        private readonly object _locker = new object();
+        private readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
         private readonly IDictionary<TKey, TValue> _values;
 
         private Func<TValue, TKey> _getKey = delegate { throw new NotImplementedException(); };
@@ -60,19 +61,24 @@ namespace FubuCore.Util
 
         public int Count
         {
-            get { return _values.Count; }
+            get
+            {
+                return readLocked(() => _values.Count);
+            }
         }
 
         public TValue First
         {
             get
             {
-                foreach (var pair in _values)
-                {
-                    return pair.Value;
-                }
-
-                return default(TValue);
+                return readLocked(() =>
+                        {
+                            foreach (var pair in _values)
+                            {
+                                return pair.Value;
+                            }
+                            return default(TValue);
+                        });
             }
         }
 
@@ -82,19 +88,22 @@ namespace FubuCore.Util
             {
                 FillDefault(key);
 
-                return _values[key];
+                return readLocked(()=> _values[key]);
             }
             set
             {
-                _onAddition(value);
-                if (_values.ContainsKey(key))
-                {
-                    _values[key] = value;
-                }
-                else
-                {
-                    _values.Add(key, value);
-                }
+                writeLocked(() =>
+                                {
+                                    _onAddition(value);
+                                    if (_values.ContainsKey(key))
+                                    {
+                                        _values[key] = value;
+                                    }
+                                    else
+                                    {
+                                        _values.Add(key, value);
+                                    }
+                                });
             }
         }
 
@@ -120,111 +129,183 @@ namespace FubuCore.Util
 
         public void Fill(TKey key, Func<TKey, TValue> onMissing)
         {
-            if (!_values.ContainsKey(key))
-            {
-                lock (_locker)
+            conditionalWriteLock(() => !_values.ContainsKey(key), () =>
                 {
-                    if (!_values.ContainsKey(key))
-                    {
-                        
-                        var value = onMissing(key);
-                        _onAddition(value);
-                        _values.Add(key, value);
-                    }
-                }
-            }
+                    var value = onMissing(key);
+                    _onAddition(value);
+                    _values.Add(key, value);
+                });
         }
 
         public void Fill(TKey key, TValue value)
         {
-            if (_values.ContainsKey(key))
-            {
-                return;
-            }
-
-            _values.Add(key, value);
+            Fill(key, k => value);
         }
 
         public void Each(Action<TValue> action)
         {
-            foreach (var pair in _values)
-            {
-                action(pair.Value);
-            }
+            readLocked(() =>
+                           {
+                               foreach (var pair in _values)
+                               {
+                                   action(pair.Value);
+                               }
+                           });
         }
 
         public void Each(Action<TKey, TValue> action)
         {
-            foreach (var pair in _values)
-            {
-                action(pair.Key, pair.Value);
-            }
+            readLocked(() =>
+                           {
+                               foreach (var pair in _values)
+                               {
+                                   action(pair.Key, pair.Value);
+                               }
+                           });
         }
 
         public bool Has(TKey key)
         {
-            return _values.ContainsKey(key);
+            return readLocked(() => _values.ContainsKey(key));
         }
 
         public bool Exists(Predicate<TValue> predicate)
         {
-            var returnValue = false;
+            return readLocked(() =>
+                           {
+                               var returnValue = false;
 
-            Each(delegate(TValue value) { returnValue |= predicate(value); });
+                               Each(delegate(TValue value) { returnValue |= predicate(value); });
 
-            return returnValue;
+                               return returnValue;
+                           });
         }
 
         public TValue Find(Predicate<TValue> predicate)
         {
-            foreach (var pair in _values)
-            {
-                if (predicate(pair.Value))
-                {
-                    return pair.Value;
-                }
-            }
+            return readLocked(() =>
+                                  {
+                                      foreach (var pair in _values)
+                                      {
+                                          if (predicate(pair.Value))
+                                          {
+                                              return pair.Value;
+                                          }
+                                      }
 
-            return default(TValue);
+                                      return default(TValue);
+                                  });
         }
 
         public TKey[] GetAllKeys()
         {
-            return _values.Keys.ToArray();
+            return readLocked(() => _values.Keys.ToArray());
         }
 
         public TValue[] GetAll()
         {
-            return _values.Values.ToArray();
+            return readLocked(() => _values.Values.ToArray());
         }
 
         public void Remove(TKey key)
         {
-            if (_values.ContainsKey(key))
-            {
-                _values.Remove(key);
-            }
+            conditionalWriteLock(() => _values.ContainsKey(key), () => _values.Remove(key));
         }
 
         public void ClearAll()
         {
-            _values.Clear();
+            writeLocked(()=>_values.Clear());
         }
 
         public bool WithValue(TKey key, Action<TValue> callback)
         {
-            if (Has(key))
-            {
-                callback(this[key]);
-                return true;
-            }
+            if( ! Has(key) ) return false;
 
-            return false;
+            callback(this[key]);
+            return true;
         }
 
         public IDictionary<TKey, TValue> ToDictionary()
         {
-            return new Dictionary<TKey, TValue>(_values);
+            return readLocked(() => new Dictionary<TKey, TValue>(_values));
         }
+
+#region lock helper methods
+        private T readLocked<T>(Func<T> func)
+        {
+            if (_lock.IsReadLockHeld) return func();
+
+            _lock.EnterReadLock();
+            try
+            {
+                return func();
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        private void readLocked(Action action)
+        {
+            readLocked<object>(() =>
+            {
+                action();
+                return null;
+            });
+        }
+
+        private T writeLocked<T>(Func<T> func)
+        {
+            if (_lock.IsWriteLockHeld)
+            {
+                return func();
+            }
+
+            // TODO: Check if( _lock.IsReadLockHeld )  ??? throw exception? Try to upgrade? What if it's not upgradeable?
+
+            _lock.EnterWriteLock();
+            try
+            {
+                return func();
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        private void writeLocked(Action action)
+        {
+            writeLocked<object>(() =>
+            {
+                action();
+                return null;
+            });
+        }
+
+        private void conditionalWriteLock(Func<bool> condition, Action action)
+        {
+            _lock.EnterUpgradeableReadLock();
+            try
+            {
+                if (!condition()) return;
+
+                _lock.EnterWriteLock();
+                try
+                {
+                    action();
+                }
+                finally
+                {
+                    _lock.ExitWriteLock();
+                }
+            }
+            finally
+            {
+                _lock.ExitUpgradeableReadLock();
+            }
+        }
+#endregion
     }
 }
